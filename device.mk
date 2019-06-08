9e0123802ecce35e1f5430397764d6427a257ab8
Soft-Bullet <40681532+Soft-Bullet@users.noreply.github.com>
2019-06-08 10:17:01 +0545

    x

--- device.mk
+++ device.mk
@@ -207,7 +207,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/sec_touchkey.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/sec_touchkey.kl \
     $(LOCAL_PATH)/keylayout/sec_touchscreen.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/sec_touchscreen.kl \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/gpio-keys.kl \
-    $(LOCAL_PATH)/keylayout/sec_touchkey.kl:system/usr/keylayout/sec_touchkey.kl \
     $(LOCAL_PATH)/keylayout/Synaptics_RMI4_TouchPad_Sensor.idc:system/usr/idc/Synaptics_RMI4_TouchPad_Sensor.idc \
     $(LOCAL_PATH)/keylayout/Synaptics_HID_TouchPad.idc:system/usr/idc/Synaptics_HID_TouchPad.idc
 
@@ -258,7 +257,6 @@ PRODUCT_PACKAGES += \
     libOmxVenc \
     libOmxVidEnc \
     libOmxVdpp
-
 
 # Misc
 PRODUCT_PACKAGES += \
